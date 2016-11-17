72103e00104fe53d081f39a0ca0df21fcc737500
Jack Storrs <jackstorrs8@gmail.com>
2016-11-17 12:02:38 -0500

    Added a create method to ImageController which adds images to the database.

--- app/Http/Controllers/ImageController.js
+++ app/Http/Controllers/ImageController.js
@@ -1,7 +1,13 @@
 'use strict'
 
+const Image = use('App/Model/Image')
 class ImageController {
+  * create (request, response) {
+      let data = request.only('title', 'description', 'url');
+      let image = yield Image.create(data);
 
+      response.json(image);
+  }
 }
 
 module.exports = ImageController
