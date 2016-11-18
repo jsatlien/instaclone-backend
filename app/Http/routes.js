b1952ab06d800f023f73ee7b811033ee48b0172f
Jack Storrs <jackstorrs8@gmail.com>
2016-11-18 11:29:40 -0500

    Add a new route for getting a json object that is a single image, add a corresponding controller method.

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -21,5 +21,7 @@ Route.on('/').render('welcome')
 
 Route.post('/images', 'ImageController.create')
 Route.get('/images', 'ImageController.show')
+
 Route.delete('/images/:id', 'ImageController.delete')
 Route.patch('/images/:id', 'ImageController.update')
+Route.get('/images/:id', 'ImageController.showSingle')
