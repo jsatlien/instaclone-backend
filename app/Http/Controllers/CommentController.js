641d56574889295389f8f76c889514515b0ac978
Jack Storrs <jackstorrs8@gmail.com>
2016-11-18 14:16:11 -0500

    Define method for adding comments to a specific image.

--- app/Http/Controllers/CommentController.js
+++ app/Http/Controllers/CommentController.js
@@ -3,7 +3,12 @@
 const Comment = use("App/Model/Comment")
 class CommentController {
   * create (request, response) {
-    
+    let imageId = request.param('id')
+    let data = request.only('content')
+    data.image_id = imageId
+    let newComment = yield Comment.create(data)
+
+    response.status(201).json(newComment)
   }
 }
 
